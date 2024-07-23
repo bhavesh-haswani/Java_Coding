85decf566cec0c4056798b089e1a6c4d35b2ee53
Bhavesh Haswani <bhaveshhaswani1902@gmail.com>
2024-07-23 11:32:29 +0530

    Creating a Graph using Adjacency List - half

--- GG/src/l37_Graphs_1/A37_1_CreateAGraphusingAdjacencyList.java
+++ GG/src/l37_Graphs_1/A37_1_CreateAGraphusingAdjacencyList.java
@@ -1,12 +1,41 @@
 package l37_Graphs_1;
 
+import java.util.*;
+
 public class A37_1_CreateAGraphusingAdjacencyList {
 
+	public class Edge {
+		
+		int source;
+		int destination;
+		int weight;
+		
+		public Edge(int source, int destination, int weight) {
+			this.source = source;
+			this.destination = destination;
+			this.weight = weight;
+		}
+		
+	}
+	
+	
 	public static void main(String[] args) {
+		int vertices = 5;
+		
+		ArrayList<Edge> graph[] = new ArrayList[vertices];
 		
 		
+		//vertex1
+		ArrayList<Integer> vertex1 = new ArrayList<>();
+		
+		//vertex2
+		ArrayList<Integer> vertex2 = new ArrayList();
+		
+		//vertex3
+		ArrayList<Integer> vertex3 = new ArrayList();
 		
 		
+				
 	}
 
 }
