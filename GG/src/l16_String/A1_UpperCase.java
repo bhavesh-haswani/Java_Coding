b369d484518794362b0d6e94e3fee8083e192094
Bhavesh Haswani <bhaveshhaswani1902@gmail.com>
2025-08-24 22:29:03 +0530

    testing UpperCase

---
+++ GG/src/l16_String/A1_UpperCase.java
@@ -0,0 +1,18 @@
+package l16_String;
+
+public class A1_UpperCase {
+	
+	public static void main(String args[]) {
+		
+		String str = "Bhavesh";
+		
+		System.out.println("str=" +str);
+		
+		str = str.toUpperCase();
+		System.out.println("str=" +str);
+		
+		
+		
+	}
+
+}
